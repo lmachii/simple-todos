af50b44b154daad17616bdfbd85fe0f9c6841d2f
Leland Machii <lelandmachii@gmail.com>
2019-03-03 13:17:19 -1000

    Section 4

--- app/imports/ui/App.js
+++ app/imports/ui/App.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
+import ReactDOM from 'react-dom';
 
 // App component - represents the whole app
 class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
   renderTasks() {
@@ -24,6 +32,13 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>
@@ -36,6 +51,6 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 })(App);
